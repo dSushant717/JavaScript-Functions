bb60bb2f7a1b598b1e1e883f322f0463cfecf780
dSushant717 <sdahal249@gmail.com>
2026-04-09 22:35:22 -0400

    fizzBuzz function with its arrow variant

---
+++ functions.js
@@ -0,0 +1,30 @@
+// 1. FizzBuzz
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+const fizzBuzzArrow = (n) => {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+};
+
+
